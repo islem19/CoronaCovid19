99c70dcae90457baa516a55ccc729f52e2300336
billel.boumessaidia <billel.boumessaidia@cevital.com>
2020-03-20 23:01:15 +0100

    update profile ui + add icon

--- app/src/main/java/dz/islem/covid19/ui/home/profile/ProfileFragment.java
+++ app/src/main/java/dz/islem/covid19/ui/home/profile/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,7 +12,6 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import dz.islem.covid19.App;
 import dz.islem.covid19.R;
 import dz.islem.covid19.Utils;
 import dz.islem.covid19.data.DataManager;
@@ -21,14 +21,15 @@ import dz.islem.covid19.ui.base.BaseFragment;
 public class ProfileFragment extends BaseFragment<ProfileViewModel> {
     private static final String TAG = "ProfileFragment";
     private ProfileViewModel viewModel;
-    @BindView(R.id.item_title)
-    TextView countryName;
-    @BindView(R.id.total_cases_value)
-    TextView totalCases;
-    @BindView(R.id.death_cases_value)
-    TextView deathCases;
-    @BindView(R.id.recover_cases_value)
-    TextView recoverCases;
+    @BindView(R.id.countryTitle)
+    TextView countryTitle;
+    @BindView(R.id.caseItem)
+    LinearLayout caseItem;
+    @BindView(R.id.deathItem)
+    LinearLayout deathItem;
+    @BindView(R.id.recoverItem)
+    LinearLayout recoverItem;
+
 
     public ProfileFragment() {
     }
@@ -69,25 +70,43 @@ public class ProfileFragment extends BaseFragment<ProfileViewModel> {
             if (country == null) return;
             setCountryName(country.getCountry());
             setTotalCases(String.valueOf(country.getNbrCases()));
-            setDeathCases(String.valueOf(country.getNbrDeath()));
+            setActiveCases(String.valueOf(country.getNbrActiveCases()));
+            setTodayCases(String.valueOf(country.getTodayCases()));
+            setTotalDeathCases(String.valueOf(country.getNbrDeath()));
+            setTodayDeathCases(String.valueOf(country.getTodayDeaths()));
             setRecoverCases(String.valueOf(country.getNbrRecovered()));
         }
     }
 
     private void setCountryName(String countryName) {
-        this.countryName.setText(countryName);
+        this.countryTitle.setText(countryName);
     }
 
     private void setTotalCases(String totalCases) {
-        this.totalCases.setText(totalCases);
+        ( (TextView) this.caseItem.findViewById(R.id.totalCaseValue)).setText(totalCases);
     }
 
-    private void setDeathCases(String deathCases) {
-        this.deathCases.setText(deathCases);
+    private void setActiveCases(String totalCases) {
+        ( (TextView) this.caseItem.findViewById(R.id.activeCaseValue)).setText(totalCases);
+    }
+
+    private void setTodayCases(String totalCases) {
+        ( (TextView) this.caseItem.findViewById(R.id.todayCaseValue)).setText(totalCases);
+    }
+
+    private void setTotalDeathCases(String deathCases) {
+
+        ( (TextView) this.deathItem.findViewById(R.id.totalDeathValue)).setText(deathCases);
+    }
+
+    private void setTodayDeathCases(String deathCases) {
+
+        ( (TextView) this.deathItem.findViewById(R.id.todayDeathValue)).setText(deathCases);
     }
 
     private void setRecoverCases(String recoverCases) {
-        this.recoverCases.setText(recoverCases);
+
+        ( (TextView) this.recoverItem.findViewById(R.id.recoverValue)).setText(recoverCases);
     }
 
 }
